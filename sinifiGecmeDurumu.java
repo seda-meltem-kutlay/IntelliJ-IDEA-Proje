0b0dd10dd6df71aa2c9c0885bc73aeae70ab1aec
sedameltemkutlay <111974816+seda-meltem-kutlay@users.noreply.github.com>
2022-08-28 19:56:25 +0300

    Add files via upload

---
+++ sinifiGecmeDurumu.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class sinifiGecmeDurumu {
+    public static void main(String[] args ){
+        int mat, fizik,kimya,turkce,muzik;
+        double average;
+    Scanner input= new Scanner(System.in);
+    System.out.print("Matematik notunuzu giriniz : ");
+    mat = input.nextInt();
+    System.out.print("Fizik notunuzu giriniz : ");
+    fizik =input.nextInt();
+    System.out.print("Kimya notunuzu giriniz :");
+    kimya = input.nextInt();
+    System.out.print("Türkçe notunuzu giriniz :");
+    turkce = input.nextInt();
+    System.out.print("Müzik notunuzu giriniz :");
+    muzik=input.nextInt();
+    if ((0 <=mat && mat <=100) && (0 <=fizik && fizik <=100) &&
+            (0 <=kimya && kimya <=100) && (0<= turkce && turkce <=100) &&
+        (0 <=muzik && muzik <=100)){
+    }else
+        average= (mat+fizik+kimya+turkce+muzik)/5;{
+
+
+
+        } {
+            System.out.println("Girdiğiniz not değerleri 0 ile 100 aralığında olmalıdır");
+        }
+    }}

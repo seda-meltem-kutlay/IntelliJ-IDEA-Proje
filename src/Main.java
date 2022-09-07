e7636965fb4892baa72ccc74779c5100e5682b54
sedameltemkutlay <111974816+seda-meltem-kutlay@users.noreply.github.com>
2022-09-07 17:05:29 +0300

    Delete Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Fighter selin = new Fighter("Selin", 15, 100, 90, 12);
-        Fighter ayse = new Fighter("Ayşe", 10, 100, 100, 100);
-
-        Ring r = new Ring(selin, ayse, 90, 100);
-        r.run();
-    }
-}
